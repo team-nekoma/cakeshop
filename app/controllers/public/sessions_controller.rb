ee477617b0c119e439583ba1434133def9a9e96c
EC2 Default User <ec2-user@ip-172-31-5-0.ap-northeast-1.compute.internal>
2023-07-17 09:38:48 +0000

    flog create

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,2 +1,12 @@
 class Public::SessionsController < ApplicationController
+    before_action :withdraw, only: [:create]
+    
+    protected
+    def withdraw
+      @customer = Customer.find_by(email: params[:customer][:email])
+      return if !@customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+        redirect_to new_customer_registration_path
+      end
+    end
 end
