6af65e09c24eaa98037dda95576caf67af7086d2
minatubouti <pingneimeinai@gmail.com>
2023-07-19 05:59:55 +0000

    itemcreate

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,20 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :status, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+ enum status: { on_sale: 0, off_sale: 1 }
 end
