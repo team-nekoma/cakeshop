c4692888cfff99763db4924f31ca918f603fa699
minatubouti <pingneimeinai@gmail.com>
2023-07-20 15:11:43 +0000

    customers/index

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,12 @@
 class Public::CustomersController < ApplicationController
+   before_action :authenticate_customer!
+
+  def withdraw
+  end
+
+  def withdraw_update
+    current_customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
 end
