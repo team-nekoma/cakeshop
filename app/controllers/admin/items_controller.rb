73bf2b2fac307f0fabefce5af3e519363cb34453
yonbok <na.re.zutomayo14@gmail.com>
2023-07-20 06:18:15 +0000

    items-index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,7 @@
 class Admin::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all.page(params[:page]).per(10)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  enum is_active: { 販売中: 1,販売停止中: 2}
+
 end
