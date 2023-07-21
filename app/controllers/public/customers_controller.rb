d91aa594ef69ba25e402b542053308355d318715
minatubouti <pingneimeinai@gmail.com>
2023-07-21 14:37:34 +0000

    customers/home/create

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,22 @@
 class Public::CustomersController < ApplicationController
    before_action :authenticate_customer!
+   
+   def show
+    @customer = current_customer
+   end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(@customer), notice: "登録情報が更新されました。"
+    else
+      render :edit
+    end
+  end
 
   def withdraw
   end
@@ -8,5 +25,11 @@ class Public::CustomersController < ApplicationController
     current_customer.update(is_deleted: true)
     reset_session
     redirect_to root_path
+  end
+  
+   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :email)
   end
 end
