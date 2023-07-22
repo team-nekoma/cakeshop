d3c09ac3d4128dfdd16c0ea1f7f3c3e177f9994a
EC2 Default User <ec2-user@ip-172-31-5-0.ap-northeast-1.compute.internal>
2023-07-22 01:33:16 +0000

    debug

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
+
  def price_including_tax
   (price * 1.1).floor  # 税率を10%として計算して切り捨て
-end
+ end
 
   has_one_attached :image
   enum status: { on_sale: 0, off_sale: 1 }
