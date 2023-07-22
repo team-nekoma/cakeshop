c44b778b4c2ee5b2c1ea9c4f8ea7463da14ea4ae
EC2 Default User <ec2-user@ip-172-31-5-0.ap-northeast-1.compute.internal>
2023-07-22 04:52:50 +0000

    cartitem

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
    has_many :orders
    has_many :cart_items, dependent: :destroy
+   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
