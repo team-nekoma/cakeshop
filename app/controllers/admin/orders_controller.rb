901b66df8e28755cc7dd2d44836554cc27e17ec5
minatubouti <pingneimeinai@gmail.com>
2023-07-25 03:23:38 +0000

    pudata

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,18 +1,15 @@
 class Admin::OrdersController < ApplicationController
    before_action :authenticate_admin!
    
-  def index
-      @orders = Order.all
-  end
   
   def update_status
     @order = Order.find(params[:id])
     if @order.update(order_params)
       flash[:notice] = "注文ステータスを更新しました"
-      redirect_to admin_orders_path
+      redirect_to admin_path
     else
       flash[:alert] = "注文ステータスの更新に失敗しました"
-      redirect_to admin_orders_path
+      redirect_to admins_path
     end
   end
 
