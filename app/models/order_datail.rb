185c4197c7b42057ad12ad2aa10d18027aca1d78
yonbok <na.re.zutomayo14@gmail.com>
2023-07-25 06:53:48 +0000

    orders-status

--- app/models/order_datail.rb
+++ app/models/order_datail.rb
@@ -1,5 +1,8 @@
 class OrderDatail < ApplicationRecord
     belongs_to :order
     belongs_to :item
-    
+
+    enum making_status: { production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
+
+
 end
