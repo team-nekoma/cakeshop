6c8f79b48bdd73cca55453c6e1e186d8190e7073
yonbok <na.re.zutomayo14@gmail.com>
2023-07-26 05:33:22 +0000

    orders-status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,19 +2,19 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @order = Order.find(params[:order_id])
-    @order_detail = OrderDetail.find(params[:id])
-    @order_details = @order.order_details.all
+    @order = Order.find(params[:id])
+    @order_detail = OrderDatail.find(params[:id])
+    @order_details = @order.order_datails.all
 
     is_updated = true
     if @order_detail.update(order_detail_params)
-      @order.update(status: 2) if @order_detail.production_status == "in_production"
+      @order.update(order_status: "in_production") if @order_detail.production_status == "in_production"
       @order_details.each do |order_detail|
         if order_detail.production_status != "production_complete"
           is_updated = false
         end
       end
-      @order.update(status: 3) if is_updated
+      @order.update(order_status: "preparing_to_ship") if is_updated
     end
     redirect_to admin_order_path(@order)
   end
@@ -23,7 +23,7 @@ class Admin::OrderDetailsController < ApplicationController
   private
 
   def order_detail_params
-    params.require(:order_detail).permit(:production_status)
+    params.require(:order_datail).permit(:production_status)
   end
 
 end
