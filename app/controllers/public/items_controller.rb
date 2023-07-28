00b7605df58fc062f8984bc4f56216dc177f4c20
minatubouti <pingneimeinai@gmail.com>
2023-07-28 06:34:49 +0000

    CSS updata

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,16 @@
 class Public::ItemsController < ApplicationController
- def index
-   @genres = Genre.all
-  if params[:genre_id].present?
-    @items = Item.where(genre_id: params[:genre_id], is_active: true)
-  else
-    @items = Item.where(is_active: true)
+  def index
+    @genres = Genre.all
+    if params[:genre_id].present?
+      @items = Item.where(genre_id: params[:genre_id], is_active: true)
+    else
+      @items = Item.where(is_active: true)
+    end
+
+    if params[:search].present?
+      @items = @items.where('name LIKE ?', "%#{params[:search]}%")
+    end
   end
- end
 
   
   def show
